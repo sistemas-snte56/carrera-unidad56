b0b0c5ab57209a3872b4612747cc37a7184459f3
sistemas-snte56 <computo.seccional@outlook.com>
2026-09-15 16:48:03 -0600

    feat: Cambio de titulo en la pagina principal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Registro Carrera Unidad 56</title>
+    <title>REGISTRO 1ra. CARRERA ATLÉTICA DEL EDUCADOR FÍSICO</title>
 
     @livewireStyles
 </head>
