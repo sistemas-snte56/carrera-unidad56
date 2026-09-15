69bc61df69cb3517732bc8cb60ba7b0463435216
sistemas-snte56 <computo.seccional@outlook.com>
2026-09-15 16:06:32 -0600

    Creacion de usuario para migracion

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use App\Models\User;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,10 +18,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@email.com',
+            'password' => bcrypt('password2026'),
+        ]);
 
         $this->call([
             RegionSeeder::class,
